3ab7532c0b99406fca71eca066a28d31d18f5d63
Dale V. Ross <dalevross@gmail.com>
2020-01-10 00:37:57 -0500

    Debugging facebook

---
+++ __d.js
@@ -0,0 +1,8 @@
+const __d = function (a, b, c, d) {
+    Z()
+        (function () {
+            G(a, b, c, (d || j) | l, null, null, null)
+        }, "define " + a, {
+            root: !0
+        })()
+}
